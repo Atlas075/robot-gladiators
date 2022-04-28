2f5bc30f21aec265a8b2be08bddc59b1eb966962
Donovan <donniemassenburg75@gmail.com>
2022-04-27 21:14:52 -0400

    added arrays to the initial game

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,13 +3,26 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Andriod", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function(){window.alert("Welcome to Robot Gladiator");
+console.log(playerName, playerAttack, playerHealth);
+console.log(enemyNames);
+
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+for(var i = 0; i < enemyNames.length; i++)
+{
+  console.log(enemyNames[i]);
+  console.log(i);
+  console.log(enemyNames[i] + "is at " +i+ " index ")
+};
+
+
+var fight = function(enemyName){window.alert("Welcome to Robot Gladiator");
 var promptFight = window.prompt("Would you like to skip this battler? Enter fight or Skip to choose")
 
 if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -29,7 +42,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove player's health by subtracting the amount set in the enemyAttack variable
     playerHealth = playerHealth - enemyAttack;
     console.log(
-      enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+      enemyNames + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
     );
   
     // check player's health
@@ -62,4 +75,7 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
   }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++)
+{
+fight(enemyNames[i]);
+}
